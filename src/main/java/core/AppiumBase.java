b16575241b74caaf73fa2c91586a05e45a289d2a
HaDTM <>
2025-04-19 10:34:07 +0700

    more beautiful

--- src/main/java/core/AppiumBase.java
+++ src/main/java/core/AppiumBase.java
@@ -2,13 +2,10 @@ package core;
 
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.options.BaseOptions;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
